617c74efb089f74c212e8a985895f210fae22071
dipdawiz <dipayan.aich@gmail.com>
2019-06-03 12:31:53 +0530

    added test cases

--- test/test.js
+++ test/test.js
@@ -1,35 +1,10 @@
-/**
- *
- * 2018-2019 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
- * Bangalore, India. All Rights Reserved.
- *
- */
+var chai = require('chai');
+chai.use(require('chai-things'));
 
-// Author : Atul
-var oecloud = require('oe-cloud');
+var bootstrapped = require('./bootstrap');
 
-oecloud.observe('loaded', function (ctx, next) {
-  return next();
-})
-
-oecloud.boot(__dirname, function (err) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-  var loopback = require('loopback');
-  oecloud.start();
-  oecloud.emit('test-start');
-});
-
-describe('Oe Passport Component Test Started', function () {
-  this.timeout(15000);
-  it('Waiting for application to start', function (done) {
-    oecloud.on('test-start', function () {
-      done();
-    });
+describe("basic tests", () => {
+  before('wait for boot', function(done){
+    bootstrapped.then(() => done());
   });
 });
-
-
-
