606bcf5d36bcb88e5bb973dbea461982791e95dd
vamsee <vamseerama_c@infosys.com>
2020-01-10 16:50:19 +0530

    removed unneeded packages and updated ci

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -2,15 +2,15 @@ var oecloud = require('oe-cloud');
 var logger = require('oe-logger');
 var log = logger('test-bootstrapper');
 
-//var loopback = require('loopback');
+// var loopback = require('loopback');
 // oecloud.attachMixinsToBaseEntity("SkeletonMixin");
 process.env.userfieldname = 'username';
 process.env.PASSWORD_FIELD_NAME = 'password';
-process.env.JWT_SECRET_KEY = 'secret'
+process.env.JWT_SECRET_KEY = 'secret';
 process.env.JWT_FOR_ACCESS_TOKEN = true;
 
 oecloud.observe('loaded', function (ctx, next) {
-  console.log("oe-cloud modules loaded");
+  console.log('oe-cloud modules loaded');
   return next();
 });
 
@@ -20,12 +20,12 @@ var testContext = {
   }
 };
 
-oecloud.get('/info',function(req, res){
-  return res.end(JSON.stringify({"status": "logged in", "access-token": res.accessToken}));
+oecloud.get('/info', function (req, res) {
+  return res.end(JSON.stringify({'status': 'logged in', 'access-token': res.accessToken}));
 });
 
-oecloud.get('/failed',function(req, res){
-  return res.end(JSON.stringify({"status": "failed"}));
+oecloud.get('/failed', function (req, res) {
+  return res.end(JSON.stringify({'status': 'failed'}));
 });
 
 oecloud.boot(__dirname, function (err) {
@@ -38,9 +38,9 @@ oecloud.boot(__dirname, function (err) {
   oecloud.emit('test-start');
 });
 
-module.exports = new Promise(booted => 
-    oecloud.on('test-start', () => {
-      //debugger;
-      //console.log('booted');
-      booted();
-    } ) );
+module.exports = new Promise(booted =>
+  oecloud.on('test-start', () => {
+    // debugger;
+    // console.log('booted');
+    booted();
+  } ) );
--- test/local-login.spec.js
+++ test/local-login.spec.js
@@ -1,14 +1,14 @@
 /**
- * 
+ *
  * ©2016-2017 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
  * Bangalore, India. All Rights Reserved.
- * 
+ *
  */
 var chalk = require('chalk');
 var bootstrap = require('./bootstrap');
 var app = require('oe-cloud');
 models = app.models;
-var defaultContext = {"ctx":{"tenantId":"default"}};
+var defaultContext = {'ctx': {'tenantId': 'default'}};
 var chai = require('chai');
 chai.use(require('chai-things'));
 var expect = chai.expect;
@@ -18,112 +18,106 @@ var api = defaults(supertest(app));
 var accessToken = '';
 
 describe(chalk.blue('model-validation PropertyLevel Validation test'), function () {
-    this.timeout(600000);
-      before('wait for boot', function(done){
-          bootstrap.then(() => {
-          // debugger
-          //create user
-            done();
-          })
-          .catch(done)
-      });
-
-      before('create user', function(done){
-        var userData = {
-            "id":"admin",
-            "username":"admin",
-            "email":"admin@ev.com",
-            "password":"admin",
-            "emailVerified": false
-            };
-        models.User.create(userData, defaultContext, function(err, user){
-            if(err) {
-                done(err);
-            } else {
-                user.updateAttributes({"emailVerified": true }, defaultContext, function(err){
-                    done(err);
-                });
-            }
-        })
-      });
-
-      after ('remove user', function(done){
-          models.User.destroyById('admin', function(err){
-            models.ModelDefinition.destroyById('tEsTmOdEl', function(err){
-                done(err)
-            })
-          });
-          
-      });
-
-      it('should login using auth/local', function(done){
-        api.post('/auth/local')
-        .set('Accept', 'application/json')
-        .send({
-          "username":"admin",
-          "password":"admin"
-          })
-          .expect(302)
-          .expect('Location', '/info')
-          .end(function(err){
-              if(err)
-                done(err)
-              
-            expect(err).to.be.null;
-            console.info('login successful');
-            done();
-          })
-      });
-
-      it('should fail to login using auth/local', function(done){
-        api.post('/auth/local')
-        .set('Accept', 'application/json')
-        .send({
-          "username":"admin",
-          "password":"admin1"
-          })
-          .expect(302)
-          .expect('Location', '/failed')
-          .end(function(err){
-              if(err)
-                done(err)
-              
-            expect(err).to.be.null;
-            console.info('login failed as expected');
-            done();
-          })
-    });
-
-    it('should contain access token which is a JWT', function(done){
-        models.User.login({"username": "admin", "password": "admin"}, function(err, res){
-            accessToken = res.accessToken;
-            expect(res.accessToken).to.be.defined;
-            expect(res.accessToken.split('.').length).equals(3);
-            done(err);
-        });
+  this.timeout(600000);
+  before('wait for boot', function (done) {
+    bootstrap.then(() => {
+      // debugger
+      // create user
+      done();
     })
+      .catch(done);
+  });
 
-    it('should successfully POST with access token which is a JWT', function(done){
-        api.post('/api/ModelDefinitions?access_token=' + accessToken)
-            .set('Accept', 'application/json')
-            .send({
-                "id": "tEsTmOdEl",
-                "name": "TestModel", 
-                "properties": {
-                    "customer name":"string",
-                    "type": "number"
-                } 
-            })
-            .expect(200)
-            .end(function(err, response){
-                if(err)
-                    done(err)
-                
-                expect(err).to.be.null;
-                expect(response.body.name).equals('TestModel');
-                done();
-            })
-    
+  before('create user', function (done) {
+    var userData = {
+      'id': 'admin',
+      'username': 'admin',
+      'email': 'admin@ev.com',
+      'password': 'admin',
+      'emailVerified': false
+    };
+    models.User.create(userData, defaultContext, function (err, user) {
+      if (err) {
+        done(err);
+      } else {
+        user.updateAttributes({'emailVerified': true }, defaultContext, function (err) {
+          done(err);
         });
+      }
+    });
+  });
 
+  after('remove user', function (done) {
+    models.User.destroyById('admin', function (err) {
+      models.ModelDefinition.destroyById('tEsTmOdEl', function (err) {
+        done(err);
+      });
+    });
+  });
+
+  it('should login using auth/local', function (done) {
+    api.post('/auth/local')
+      .set('Accept', 'application/json')
+      .send({
+        'username': 'admin',
+        'password': 'admin'
+      })
+      .expect(302)
+      .expect('Location', '/info')
+      .end(function (err) {
+        if (err) {done(err);}
+
+        expect(err).to.be.null;
+        console.info('login successful');
+        done();
+      });
+  });
+
+  it('should fail to login using auth/local', function (done) {
+    api.post('/auth/local')
+      .set('Accept', 'application/json')
+      .send({
+        'username': 'admin',
+        'password': 'admin1'
+      })
+      .expect(302)
+      .expect('Location', '/failed')
+      .end(function (err) {
+        if (err) {done(err);}
+
+        expect(err).to.be.null;
+        console.info('login failed as expected');
+        done();
+      });
+  });
+
+  it('should contain access token which is a JWT', function (done) {
+    models.User.login({'username': 'admin', 'password': 'admin'}, function (err, res) {
+      accessToken = res.accessToken;
+      expect(res.accessToken).to.be.defined;
+      expect(res.accessToken.split('.').length).equals(3);
+      done(err);
+    });
+  });
+
+  it('should successfully POST with access token which is a JWT', function (done) {
+    api.post('/api/ModelDefinitions?access_token=' + accessToken)
+      .set('Accept', 'application/json')
+      .send({
+        'id': 'tEsTmOdEl',
+        'name': 'TestModel',
+        'properties': {
+          'customer name': 'string',
+          'type': 'number'
+        }
+      })
+      .expect(200)
+      .end(function (err, response) {
+        if (err) {done(err);}
+
+        expect(err).to.be.null;
+        expect(response.body.name).equals('TestModel');
+        done();
+      });
+  });
 });
